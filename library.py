2a9a0963f46d9f305ce1540c929da8a667c18b48
Sandra Prosevski <prosevskisandra@gmail.com>
2026-03-15 21:02:17 +0100

    Added add_book and show_books functions

--- library.py
+++ library.py
@@ -7,4 +7,18 @@ class Book:
         return f"{self.title} - {self.author} (ISBN: {self.isbn})"
 #Lista u koju cemo spremati knjige
 library_inventory = []
-print("Sistem za upravljanje bibliotekom je prekinut.")
+#Funkcija za dodavanje knjige
+def add_book(title,author,isbn):
+    new_book = Book(title,author,isbn)
+    library_inventory.append(new_book)
+    print(f"Knjiga dodata: {new_book}")
+#Funkcija za prikaz svih knjiga
+def show_books():
+    print("\nTrenutno u biblioteci:")
+    for book in library_inventory:
+        print(book)
+#Testiranje koda
+if __name__ == "__main__":
+    add_book("Na Drini ćuprija","Ivo Andrić","123456")
+    add_book("Seobe","Miloš Crnjanski","789012")
+    show_books()
